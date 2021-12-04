da0d4fff99adc93728b282010a9be15c3b9ad245
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-05 05:58:00 +0900

    👕 フォーマッタかけ忘れ

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    }
-  })],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
 })
