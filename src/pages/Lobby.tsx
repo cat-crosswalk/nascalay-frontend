9c0e6e92ce354393e87bfcdca27c51244fca8e7a
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-08 01:09:04 +0900

    websocketの接続を記述

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppSelector } from '/@/store/hooks'
+import { setupWebSocket } from '/@/websocket'
 
 // 待機部屋(ROOM)
 const Lobby = () => {
+  const userId = useAppSelector((state) => state.user.userId)
+  useEffect(() => {
+    // websocket接続
+    setupWebSocket(userId)
+  }, [])
+
   return (
     <div>
       <h1>Lobby</h1>
