88653e392df8ff27593f119bab28fbe91f8969b1
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-07 21:51:43 +0900

    👕 yarn fix
    やっぱ lint-staged うまく働いてない気がする

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -11,36 +11,36 @@ const Canvas: React.VFC<Props> = () => {
   const [isMouseDown, setMouseDown] = useState(false)
   const [lastPos, setLastPos] = useState({ x: 0, y: 0 })
 
-  const getPos = useCallback(
-    (e: React.MouseEvent<HTMLCanvasElement>) => {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const rect = canvasRef.current!.getBoundingClientRect()
-      return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top,
-      }
-    },
-    []
-  )
+  const getPos = useCallback((e: React.MouseEvent<HTMLCanvasElement>) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const rect = canvasRef.current!.getBoundingClientRect()
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    }
+  }, [])
   const mouseDown = useCallback((e: React.MouseEvent<HTMLCanvasElement>) => {
     setLastPos(getPos(e))
     setMouseDown(true)
   }, [])
   const mouseUp = useCallback(() => setMouseDown(false), [])
-  const mouseMove = useCallback((e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!isMouseDown) return
-    const pos = getPos(e)
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const ctx = canvasRef.current!.getContext('2d')!
-    ctx.fillStyle = '#ff0000'
-    ctx.beginPath()
-    ctx.moveTo(lastPos.x, lastPos.y)
-    ctx.lineTo(pos.x, pos.y)
-    ctx.stroke()
-    ctx.closePath()
+  const mouseMove = useCallback(
+    (e: React.MouseEvent<HTMLCanvasElement>) => {
+      if (!isMouseDown) return
+      const pos = getPos(e)
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const ctx = canvasRef.current!.getContext('2d')!
+      ctx.fillStyle = '#ff0000'
+      ctx.beginPath()
+      ctx.moveTo(lastPos.x, lastPos.y)
+      ctx.lineTo(pos.x, pos.y)
+      ctx.stroke()
+      ctx.closePath()
 
-    setLastPos(pos)
-  }, [isMouseDown, lastPos])
+      setLastPos(pos)
+    },
+    [isMouseDown, lastPos]
+  )
 
   return (
     <div>
