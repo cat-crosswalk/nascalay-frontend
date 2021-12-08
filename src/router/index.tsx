bec8dd2940f8bf9f8289a410d470d4540a296394
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-09 03:58:43 +0900

    Roomに入っていなかったらトップページへ

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
 import { Routes } from 'react-router'
-import { Route } from 'react-router-dom'
+import { Route, Navigate } from 'react-router-dom'
+import { useCallback } from 'react'
+import { useAppSelector } from '/@/store/hooks'
 
 import Hello from '/@/pages/Hello'
 import Index from '/@/pages/Index'
@@ -10,14 +12,22 @@ import Answer from '/@/pages/Answer'
 import Result from '/@/pages/Result'
 
 const Router = () => {
+  const isJoinRoom = useAppSelector((state) => state.status.isJoinRoom)
+  const checkJoin = useCallback(
+    (element: React.ReactElement) => {
+      return isJoinRoom ? element : <Navigate to="/" />
+    },
+    [isJoinRoom]
+  )
+
   return (
     <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/lobby" element={<Lobby />} />
-      <Route path="/theme" element={<Theme />} />
-      <Route path="/draw" element={<Draw />} />
-      <Route path="/answer" element={<Answer />} />
-      <Route path="/result" element={<Result />} />
+      <Route path="/lobby" element={checkJoin(<Lobby />)} />
+      <Route path="/theme" element={checkJoin(<Theme />)} />
+      <Route path="/draw" element={checkJoin(<Draw />)} />
+      <Route path="/answer" element={checkJoin(<Answer />)} />
+      <Route path="/result" element={checkJoin(<Result />)} />
       <Route path="/hello/:name" element={<Hello />} />
     </Routes>
   )
