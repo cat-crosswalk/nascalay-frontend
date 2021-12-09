3c96d587b0bece09bf3b1c3b6ff7ad4983fb8f90
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-10 07:16:57 +0900

    サーバーとの接続テスト用に修正
    golangのbasepathを/apiに変更すると，これでwsやAPIが接続できる

--- src/utils/wsSetting.ts
+++ src/utils/wsSetting.ts
@@ -1,2 +1,2 @@
-export const WEBSOCKET_ENDPOINT = '/ws/test'
-export const BASE_URL = 'http://localhost:1880'
+export const WEBSOCKET_ENDPOINT = '/api/ws'
+export const BASE_URL = 'http://localhost:8080'
