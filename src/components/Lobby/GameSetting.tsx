cd4d34faf023a4cb8dec795cdfec10143cdd64ea
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-10 01:26:44 +0900

    Revert "test"
    This reverts commit ec3b8d0ef18becd987ac136eed8b46dba78bf0d0.

--- src/components/Lobby/GameSetting.tsx
+++ src/components/Lobby/GameSetting.tsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
-import { css } from '@emotion/react'
 
 const GameSetting = () => {
-  const marks = {  
-    0: <p>みじかい</p>,
-    1: <p>ふつう</p>,  
-    2: <p>ながい</p>,
-  }
-  const style = css`
-    max-width:50%;
-    margin-bottom:30px;
-    padding: 30px;
-  `
   return (
     <div>
       <div>
         <h2>ゲーム設定</h2>
-        <div css={style}>
+        <div>
           <p>時間設定</p>
-          <Slider min={0} max={2} marks={marks} step={1} defaultValue={1}/>
         </div>
       </div>
       <div>
