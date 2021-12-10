84449330f4e4385c09183a7d7eccbb8952d3e87b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-11 07:33:16 +0900

    ✨ 雑にツール類追加した

--- src/pages/Draw/index.tsx
+++ src/pages/Draw/index.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react'
 import { mdiEraser } from '@mdi/js'
 import Icon from '@mdi/react'
 import React, { useCallback, useState } from 'react'
@@ -6,6 +7,7 @@ import MainCanvas, {
   Handler as MainCanvasHandler,
   Props as MainCanvasProps,
 } from './MainCanvas'
+import ToolButton from './ToolButton'
 
 // 絵を描くページ
 const Draw = () => {
@@ -37,6 +39,71 @@ const Draw = () => {
     <div>
       <h1>Draw</h1>
       <ColorPallet onChange={setPenColor} />
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: 64px 64px 64px;
+          grid-row-gap: 20px;
+          grid-column-gap: 20px;
+        `}
+      >
+        <input
+          css={css`
+            display: none;
+          `}
+          type="radio"
+          name="toolType"
+          id="pen"
+          checked={penType === 'pen'}
+          onChange={() => setPenType('pen')}
+        />
+        <label htmlFor="pen">
+          <ToolButton
+            type="pen"
+            color={penColor}
+            isSelected={penType === 'pen'}
+          />
+        </label>
+        <input
+          css={css`
+            display: none;
+          `}
+          type="radio"
+          name="toolType"
+          id="eraser"
+          checked={penType === 'eraser'}
+          onChange={() => setPenType('eraser')}
+        />
+        <label htmlFor="eraser">
+          <ToolButton
+            type="eraser"
+            color={penColor}
+            isSelected={penType === 'eraser'}
+          />
+        </label>
+        <input
+          css={css`
+            display: none;
+          `}
+          type="radio"
+          name="toolType"
+          id="bucket"
+          checked={penType === 'bucket'}
+          onChange={() => setPenType('bucket')}
+        />
+        <label htmlFor="bucket">
+          <ToolButton
+            type="bucket"
+            color={penColor}
+            isSelected={penType === 'bucket'}
+          />
+        </label>
+
+        <ToolButton type="undo" onClick={undo} />
+        <ToolButton type="redo" onClick={redo} />
+        <ToolButton type="clear" onClick={clearCanvas} />
+      </div>
+
       <div onKeyDown={shortcut} tabIndex={-1}>
         <MainCanvas
           ref={canvasRef}
