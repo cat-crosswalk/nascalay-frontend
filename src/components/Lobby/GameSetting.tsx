017c068e2ed7802134a440a60df52dcac9a65ce4
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-10 23:55:34 +0900

    ざっくり作成

--- src/components/Lobby/GameSetting.tsx
+++ src/components/Lobby/GameSetting.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { css } from '@emotion/react'
+import Icon from '@mdi/react'
+import { mdiClipboardMultiple } from '@mdi/js'
 import CustomSlider from './Slider'
-import { card, gameCardColor } from '/@/styles'
+import { card, gameCardColor, borderColor, accentColor } from '/@/styles'
+
+import { wsSend } from '/@/websocket'
 
 const GameSetting = () => {
   const [time, setTime] = useState(1)
   const handleChange = (event: Event, newValue: number | number[]) => {
     setTime(newValue as number)
   }
+  const isHost = true // TODO
+  const inviteUrl = 'https://www.google.com' // TODO
+
+  const requestGameStart = useCallback(() => {
+    wsSend.requestGameStart()
+  }, [])
+
   const marks = [
     {
       value: 0,
@@ -26,7 +37,7 @@ const GameSetting = () => {
     <div css={[containerStyle, card]}>
       <div>
         <h2 css={titleStyle}>ゲーム設定</h2>
-        <div css={timeSettingStyle}>
+        <div>
           <p css={subTitleStyle}>時間設定</p>
           <CustomSlider
             defaultValue={1}
@@ -40,27 +51,71 @@ const GameSetting = () => {
       </div>
       <div>
         <h2 css={titleStyle}>招待</h2>
-        <input type="text" placeholder="招待コードを入力" />
-        <button>Copy</button>
+        <div css={inviteWrapStyle}>
+          <div css={urlStyle}>{inviteUrl}</div>
+          <button css={copyButtonStyle}><Icon path={mdiClipboardMultiple} size={1.8}/></button>
+        </div>
       </div>
-      <button>スタート</button>
+      {isHost &&
+      <div css={startButtonStyle}>
+        <button onClick={requestGameStart}>スタート</button>
+      </div>
+      }
     </div>
   )
 }
 
-const timeSettingStyle = css``
-
 const titleStyle = css`
   font-size: 2rem;
-  line-height: 4rem;
+  line-height: 6rem;
 `
 const subTitleStyle = css`
   font-size: 1.5rem;
+  line-height: 3rem;
 `
 
 const containerStyle = css`
   background-color: ${gameCardColor};
-  padding: 36px;
+  padding: 36px 48px;
+  padding-top: 12px;
+`
+
+const copyButtonStyle = css`
+  border: solid 3px ${borderColor};
+  background-color: #96A0C0;
+  width: 64px;
+  height: 64px;
+`
+
+const urlStyle = css`
+  background-color: ${accentColor};
+  border: solid 3px ${borderColor};
+  height: 64px;
+  width: 100%;
+  margin-right: 20px;
+`
+
+const inviteWrapStyle = css`
+  display: flex;
+  font-size: 1.5rem;
+  text-align: center;
+  line-height: 64px;
+  vertical-align: baseline;
+`
+
+const startButtonStyle = css`
+  display: flex;
+  justify-content: center;
+  margin-top: 3rem;
+  & button {
+    font-size: 2rem;
+    background-color: #96A0C0;
+    border: solid 3px ${borderColor};
+    height: 80px;
+    line-height: 80px;
+    vertical-align: middle;
+    padding: 0 4rem;
+  }
 `
 
 export default GameSetting
