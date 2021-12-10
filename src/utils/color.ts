e24b83aa2875cecf2c6fee65871483849d5653d3
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-11 01:02:23 +0900

    ♻️ white と black も追加した

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,4 +1,4 @@
-export type ColorType = 'yellow' | 'red' | 'blue'
+export type ColorType = 'yellow' | 'red' | 'blue' | 'white' | 'black'
 
 export const colorToRgb: Record<
   ColorType,
@@ -7,4 +7,6 @@ export const colorToRgb: Record<
   yellow: 'rgb(246, 223, 147)',
   red: 'rgb(242, 154, 140)',
   blue: 'rgb(160, 167, 233)',
+  white: 'rgb(246, 246, 246)',
+  black: 'rgb(0, 0, 0)',
 }
