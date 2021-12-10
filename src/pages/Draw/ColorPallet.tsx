e88a91bf8dfe4f3a178d5daf525bbda44d8d244f
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-11 05:10:02 +0900

    👕 lint

--- src/pages/Draw/ColorPallet.tsx
+++ src/pages/Draw/ColorPallet.tsx
@@ -69,13 +69,15 @@ const palletColor: { hex: `#${string}`; whiteLike?: boolean }[] = [
 ]
 
 type Props = {
-  value?: number,
-  onChange?: (color: typeof palletColor[number]['hex']) => void,
+  value?: number
+  onChange?: (color: typeof palletColor[number]['hex']) => void
 }
 
 const ColorPallet = (props: Props) => {
   const [vivus, setVivus] = React.useState<Vivus | null>(null)
-  const [selectedColorIndex, setSelectedColorIndex] = React.useState<number>(props.value ?? 11)
+  const [selectedColorIndex, setSelectedColorIndex] = React.useState<number>(
+    props.value ?? 11
+  )
   const animDuration = 50
   const changeSelectedColor = useCallback(
     (index: number) => {
@@ -86,15 +88,15 @@ const ColorPallet = (props: Props) => {
       vivus?.reset()
       vivus?.play()
     },
-    [props, vivus],
+    [props, vivus]
   )
 
   useEffect(() => {
     if (props.onChange) {
       props.onChange(palletColor[selectedColorIndex].hex)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     setVivus(
@@ -168,15 +170,21 @@ const ColorPallet = (props: Props) => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke={palletColor[selectedColorIndex].whiteLike ? '#000' : '#fff'}
+              stroke={
+                palletColor[selectedColorIndex].whiteLike ? '#000' : '#fff'
+              }
               d="M16 19.5157C17.8324 17.7372 19.9414 16.1792 22.2217 15.0201C23.0267 14.6108 24.4002 13.8809 25.3726 14.0166C26.2612 14.1406 25.101 15.4949 24.9211 15.7627C23.6127 17.71 22.3355 19.7318 21.1981 21.7836C20.6488 22.7746 20.3011 23.8115 20.0541 24.9145C19.8189 25.9652 20.0406 25.5815 20.5157 25.0952C21.3015 24.2908 22.3463 23.7605 23.2854 23.1684C25.2146 21.9522 27.0636 20.6171 28.9953 19.4053C31.5217 17.8204 38.9922 12.9353 36.672 14.8093C34.6924 16.4082 33.126 18.2346 31.5341 20.2182C29.4924 22.7623 27.07 25.5473 25.8342 28.6074C25.6274 29.1196 25.7182 29.5708 26.2657 29.1292C28.0655 27.6778 29.6778 25.992 31.5843 24.6737C32.8039 23.8303 34.1231 23.1223 35.4076 22.3857C35.6567 22.2429 38.0524 20.5155 38.4983 20.6798C38.5551 20.7007 36.982 22.9886 36.9128 23.0882C36.2852 23.9918 35.6909 24.9186 35.0865 25.8377C34.3945 26.89 33.7266 27.9546 33.0694 29.0289C32.8215 29.4342 31.8381 30.3112 32.3068 30.233C32.602 30.1838 32.8639 29.8347 33.0795 29.6611C33.9253 28.9797 34.9086 28.3393 35.9194 27.935C36.9483 27.5235 36.6335 28.5113 36.3709 29.0891C35.9334 30.0516 36.9733 31.488 37.3142 32.3404"
-              strokeWidth={palletColor[selectedColorIndex].whiteLike ? '5' : '3'}
+              strokeWidth={
+                palletColor[selectedColorIndex].whiteLike ? '5' : '3'
+              }
               strokeLinecap="round"
             />
             <path
               stroke="#fff"
               d="M16 19.5157C17.8324 17.7372 19.9414 16.1792 22.2217 15.0201C23.0267 14.6108 24.4002 13.8809 25.3726 14.0166C26.2612 14.1406 25.101 15.4949 24.9211 15.7627C23.6127 17.71 22.3355 19.7318 21.1981 21.7836C20.6488 22.7746 20.3011 23.8115 20.0541 24.9145C19.8189 25.9652 20.0406 25.5815 20.5157 25.0952C21.3015 24.2908 22.3463 23.7605 23.2854 23.1684C25.2146 21.9522 27.0636 20.6171 28.9953 19.4053C31.5217 17.8204 38.9922 12.9353 36.672 14.8093C34.6924 16.4082 33.126 18.2346 31.5341 20.2182C29.4924 22.7623 27.07 25.5473 25.8342 28.6074C25.6274 29.1196 25.7182 29.5708 26.2657 29.1292C28.0655 27.6778 29.6778 25.992 31.5843 24.6737C32.8039 23.8303 34.1231 23.1223 35.4076 22.3857C35.6567 22.2429 38.0524 20.5155 38.4983 20.6798C38.5551 20.7007 36.982 22.9886 36.9128 23.0882C36.2852 23.9918 35.6909 24.9186 35.0865 25.8377C34.3945 26.89 33.7266 27.9546 33.0694 29.0289C32.8215 29.4342 31.8381 30.3112 32.3068 30.233C32.602 30.1838 32.8639 29.8347 33.0795 29.6611C33.9253 28.9797 34.9086 28.3393 35.9194 27.935C36.9483 27.5235 36.6335 28.5113 36.3709 29.0891C35.9334 30.0516 36.9733 31.488 37.3142 32.3404"
-              strokeWidth={palletColor[selectedColorIndex].whiteLike ? '3' : '3'}
+              strokeWidth={
+                palletColor[selectedColorIndex].whiteLike ? '3' : '3'
+              }
               strokeLinecap="round"
             />
           </svg>
