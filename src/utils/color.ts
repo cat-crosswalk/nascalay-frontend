f41cb249ac3145fafbd3e512f3d53f283d886874
tararira <tararira03@gmail.com>
2021-12-12 00:23:44 +0900

    change color black

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -8,5 +8,5 @@ export const colorToRgb: Record<
   red: 'rgb(242, 154, 140)',
   blue: 'rgb(160, 167, 233)',
   white: 'rgb(246, 246, 246)',
-  black: 'rgb(0, 0, 0)',
+  black: 'rgb(14, 14, 14)',
 }
