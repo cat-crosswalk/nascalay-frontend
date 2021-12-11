236647d302a3a36688d1588ac772c29fd85aace8
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-11 23:36:04 +0900

    画像表示を変更

--- src/pages/Answer/AnswerCanvas.tsx
+++ src/pages/Answer/AnswerCanvas.tsx
@@ -1,12 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { css } from '@emotion/react'
 
 import { card } from '/@/utils/card'
+import { useAppSelector } from '/@/store/hooks'
 
 const AnswerCanvas = () => {
   const canvasWidth = 400
   const canvasHeight = 400
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const imageData = useAppSelector(state => state.answer.img)
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const canvas = canvasRef.current!
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const ctx = canvas.getContext('2d')!
+    if (imageData === '') {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+      return
+    }
+    const img = new Image()
+    img.src = imageData
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight)
+    }
+  }, [imageData])
   return (
     <div css={[container]}>
       <canvas
