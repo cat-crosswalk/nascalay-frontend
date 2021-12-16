e82656c499f963bb019ffb66897b90de22be79d4
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-16 14:50:22 +0900

    型でnullをまとめて書いた

--- src/store/slice/result.ts
+++ src/store/slice/result.ts
@@ -1,13 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { User } from '/@/utils/apis/generated'
 
-export interface Result {
-  img: string | null
-  answerer: User | null
-  sender: User | null
-  answer: string | null
-  odai: string | null
-}
+type Nullable<T> = { [key in keyof T]: T[key] | null }
+export type Result = Nullable<{
+  img: string
+  answerer: User
+  sender: User
+  answer: string
+  odai: string
+}>
 
 const initialState: Result = {
   img: null,
@@ -21,19 +22,19 @@ export const resultSlice = createSlice({
   name: 'result',
   initialState,
   reducers: {
-    setResultImage(state, action: PayloadAction<string | null>) {
+    setResultImage(state, action: PayloadAction<Result['img']>) {
       return { ...state, img: action.payload }
     },
-    setResultAnswerer(state, action: PayloadAction<User | null>) {
+    setResultAnswerer(state, action: PayloadAction<Result['answerer']>) {
       return { ...state, answerer: action.payload }
     },
-    setResultSender(state, action: PayloadAction<User | null>) {
+    setResultSender(state, action: PayloadAction<Result['sender']>) {
       return { ...state, sender: action.payload }
     },
-    setResultAnswer(state, action: PayloadAction<string | null>) {
+    setResultAnswer(state, action: PayloadAction<Result['answer']>) {
       return { ...state, answer: action.payload }
     },
-    setResultOdai(state, action: PayloadAction<string | null>) {
+    setResultOdai(state, action: PayloadAction<Result['odai']>) {
       return { ...state, odai: action.payload }
     },
   },
