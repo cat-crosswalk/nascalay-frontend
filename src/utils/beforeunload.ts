8ca24eb86c9ae11497d2e5ebe5e566048242a901
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-29 02:36:44 +0900

    📚 unload に example 足した

--- src/utils/beforeunload.ts
+++ src/utils/beforeunload.ts
@@ -3,10 +3,28 @@ const handler = (event: BeforeUnloadEvent) => {
   event.returnValue = ''
 }
 
+/**
+ * @example
+ * useEffect(() => {
+ *   setBeforeUnload()
+ *   return () => {
+ *     removeBeforeUnload()
+ *   }
+ * }
+ */
 export const setBeforeUnload = () => {
   window.onbeforeunload = handler
 }
 
+/**
+ * @example
+ * useEffect(() => {
+ *   setBeforeUnload()
+ *   return () => {
+ *     removeBeforeUnload()
+ *   }
+ * }
+ */
 export const removeBeforeUnload = () => {
   window.onbeforeunload = null
 }
