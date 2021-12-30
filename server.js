828e44667617bb2b5e92d37b99720cf01a5cb480
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-12-30 15:24:15 +0900

    ⚙️ path.join 使うようにした

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 const express = require('express')
 const history = require('connect-history-api-fallback')
 const favicon = require('serve-favicon')
+const path = require('path')
 
 const app = express()
 app.use(history())
-app.use(express.static(__dirname + '/dist'))
-app.use(favicon(__dirname + '/src/favicon.svg'))
+app.use(express.static(path.join(__dirname, '/dist')))
+app.use(favicon(path.join(__dirname, '/src/favicon.svg')))
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started :' + port)
